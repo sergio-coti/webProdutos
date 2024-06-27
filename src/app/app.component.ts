4b9df6a66601e8f2a592bbb0f5735b4ec4a15dd1
sergio-coti <cotiaulajava@outlook.com>
2024-06-27 12:33:41 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'webProdutos';
+export class AppComponent implements OnInit {
+
+  //atributos
+  produtos : any[] = [];
+
+  //declarando um objeto HttpClient
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {    
+    
+    //fazendo uma requisição para o serviço de consulta de produtos
+    this.httpClient.get('http://localhost:8081/api/produtos')
+      .subscribe({ //aguardando a resposta
+        next: (data) => { //capturando o retorno de sucesso da API
+          //armazenando os dados dos produtos obtidos da API
+          this.produtos = data as any[];
+        },
+        error: (e) => { //capturando o retorno de erro da API
+          console.log(e.error);
+        }
+      })
+  }
 }
